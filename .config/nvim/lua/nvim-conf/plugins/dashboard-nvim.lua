fb82b7fa1e404f9c26d6ca753a799b12efe55528
Thomas <259386030+ThomasReal@users.noreply.github.com>
2026-02-22 14:30:44 +0100

    Added license

--- .config/nvim/lua/nvim-conf/plugins/dashboard-nvim.lua
+++ .config/nvim/lua/nvim-conf/plugins/dashboard-nvim.lua
@@ -1,2 +1,7 @@
 vim.pack.add({ "https://github.com/nvimdev/dashboard-nvim" })
-require("dashboard").setup({})
+require("dashboard").setup({
+  letter_list = "arstoinwfydvhk",
+  config = {
+    project = { action = "FzfLua files cwd=" },
+  },
+})
